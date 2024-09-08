b0d7572b7cdb4be17cc0daf3b4a226567711a12e
minhaz1010 <minhazrahman1010@gmail.com>
2024-09-08 11:30:56 +0600

    add prettier and add shadcn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily:{
-        protest:['Protest Guerrilla','sans-serif'],
-        handjet:['Handjet','sans-serif']
-      }
-    },
+  	extend: {
+  		fontFamily: {
+  			protest: ['Protest Guerrilla','sans-serif'],
+  			handjet: ['Handjet','sans-serif']
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {}
+  	}
   },
-  plugins: [],
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  plugins: [require("tailwindcss-animate")],
 }

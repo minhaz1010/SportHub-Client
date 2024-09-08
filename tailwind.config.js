9e5fb34b3bebdc6f00d27529ecf5d2d10638bab7
minhaz1010 <minhazrahman1010@gmail.com>
2024-09-08 10:21:21 +0600

    add custom font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        protest:['Protest Guerrilla','sans-serif'],
+        handjet:['Handjet','sans-serif']
+      }
+    },
   },
   plugins: [],
 }

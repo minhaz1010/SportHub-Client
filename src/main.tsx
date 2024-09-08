1377cc66d3daf0102571a3e69296dc03a4c29bb6
minhaz1010 <minhazrahman1010@gmail.com>
2024-09-08 16:00:03 +0600

    configure redux toolkit and base api of rtk query

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import AppRoutes from './routes/AppRouter.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import AppRoutes from "./routes/AppRouter.tsx";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.ts";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-     <AppRoutes/>
+    <Provider store={store}>
+      <AppRoutes />
+    </Provider>
   </StrictMode>,
-)
+);

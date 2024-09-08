8a2dbf7915fe71590660b83ad5bb634dd962c1d5
minhaz1010 <minhazrahman1010@gmail.com>
2024-09-08 10:34:22 +0600

    add basic layout and test it

---
+++ src/routes/AppRouter.tsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+import ErrorElement from "../components/ErrorElement/ErrorElement";
+import Home from "../pages/Home";
+
+
+const router  = createBrowserRouter([
+  {
+    path:"/",
+    element:<MainLayout/>,
+    errorElement:<ErrorElement/>,
+    children:[
+      {
+        path:"/",
+        element:<Home/>
+      }
+    ]
+  }
+])
+
+const AppRoutes = () => <RouterProvider router={router} />
+export default AppRoutes
